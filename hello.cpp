1f1905afd422484017077483de6968442c630842
LRLVEC <mjdyx@outlook.com>
2023-12-09 19:47:21 +0800

    todo

--- hello.cpp
+++ hello.cpp
@@ -1,18 +1,125 @@
 #include <GLFW/glfw3.h>
 #include <webgpu/webgpu_cpp.h>
+#include <cstdio>
+// #define __EMSCRIPTEN__
 #if defined(__EMSCRIPTEN__)
 #include <emscripten/emscripten.h>
 #else
 #include <webgpu/webgpu_glfw.h>
 #endif
 
-wgpu::Instance instance;
-wgpu::Device device;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+
+// Function to perform Quick Sort
+void quickSort(std::vector<int>& arr, int low, int high)
+{
+	if (low < high)
+	{
+		int pivot = arr[high];
+		int i = low - 1;
+
+		for (int j = low; j <= high - 1; ++j)
+		{
+			if (arr[j] < pivot)
+			{
+				++i;
+				std::swap(arr[i], arr[j]);
+			}
+		}
+
+		std::swap(arr[i + 1], arr[high]);
+
+		int pivotIndex = i + 1;
+
+		quickSort(arr, low, pivotIndex - 1);
+		quickSort(arr, pivotIndex + 1, high);
+	}
+}
+
+void sort_bench()
+{
+	uint32_t n = 1000000;
+	// Reset the vector
+	std::vector<int>data(n);
+	std::generate(data.begin(), data.end(), [n]() { return rand() % n; });
+
+	// Benchmark Quick Sort
+	auto quick_start = std::chrono::high_resolution_clock::now();
+	quickSort(data, 0, data.size() - 1);
+	auto quick_end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> quick_duration = quick_end - quick_start;
+
+	// Reset the vector
+	data = std::vector<int>(n);
+	std::generate(data.begin(), data.end(), [n]() { return rand() % n; });
+
+	// Benchmark Standard Sort
+	auto std_sort_start = std::chrono::high_resolution_clock::now();
+	std::sort(data.begin(), data.end());
+	auto std_sort_end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> std_sort_duration = std_sort_end - std_sort_start;
+
+	// Output benchmark results
+	std::cout << "Quick Sort Time: " << quick_duration.count() << " seconds\n";
+	std::cout << "Standard Sort Time: " << std_sort_duration.count() << " seconds\n";
+}
+
+
+wgpu::Instance instance; // the instance of wgpu
+wgpu::Device device; //	the gpu device
 wgpu::RenderPipeline pipeline;
 
 wgpu::SwapChain swapChain;
 const uint32_t kWidth = 512;
 const uint32_t kHeight = 512;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	switch (key)
+	{
+	case GLFW_KEY_ESCAPE:
+		if (action == GLFW_PRESS)
+			glfwSetWindowShouldClose(window, true);break;
+	default:
+		break;
+	}
+}
+
+void shader_compilation_callback(
+	WGPUCompilationInfoRequestStatus status,
+	WGPUCompilationInfo const* info,
+	void* userdata)
+{
+	printf("compilation info request: ");
+	switch (status)
+	{
+	case WGPUCompilationInfoRequestStatus_Success: printf("success!");break;
+	case WGPUCompilationInfoRequestStatus_Error: printf("error!");break;
+	case WGPUCompilationInfoRequestStatus_DeviceLost: printf("device lost!");break;
+	case WGPUCompilationInfoRequestStatus_Unknown: printf("unknown!");break;
+	case WGPUCompilationInfoRequestStatus_Force32: printf("force32!");break;
+	}
+	printf("\n");
+	printf("info message count: %llu\n", info->messageCount);
+	if (info->messageCount && info->messages && info->messages->message)
+	{
+		printf("[line %llu pos %llu] ",
+			info->messages->lineNum,
+			info->messages->linePos);
+		switch (info->messages->type)
+		{
+		case WGPUCompilationMessageType_Error: printf("error");break;
+		case WGPUCompilationMessageType_Warning: printf("warning");break;
+		case WGPUCompilationMessageType_Info: printf("info");break;
+		case WGPUCompilationMessageType_Force32: printf("force_32");break;
+		}
+		printf(": %s\n",
+			info->messages->message);
+	}
+}
 
 void SetupSwapChain(wgpu::Surface surface)
 {
@@ -52,11 +159,13 @@ void GetDevice(void (*callback)(wgpu::Device))
 
 const char shaderCode[] = R"(
     @vertex fn vertexMain(@builtin(vertex_index) i : u32) ->
-      @builtin(position) vec4f {
-        const pos = array(vec2f(0, 1), vec2f(-1, -1), vec2f(1, -1));
+      @builtin(position) vec4f
+	{
+        const pos = array(vec2f(0, 0.5f), vec2f(-1, -1), vec2f(1, -1));
         return vec4f(pos[i], 0, 1);
     }
-    @fragment fn fragmentMain() -> @location(0) vec4f {
+    @fragment fn fragmentMain() -> @location(0) vec4f
+	{
         return vec4f(1, 0, 0, 1);
     }
 )";
@@ -67,11 +176,12 @@ void CreateRenderPipeline()
 	wgslDesc.code = shaderCode;
 
 	wgpu::ShaderModuleDescriptor shaderModuleDescriptor{ .nextInChain = &wgslDesc };
-	wgpu::ShaderModule shaderModule =
-		device.CreateShaderModule(&shaderModuleDescriptor);
+	wgpu::ShaderModule shaderModule = device.CreateShaderModule(&shaderModuleDescriptor);
 
+#if !defined(__EMSCRIPTEN__)
+	shaderModule.GetCompilationInfo(shader_compilation_callback, nullptr);
+#endif
 	wgpu::ColorTargetState colorTargetState{ .format = wgpu::TextureFormat::BGRA8Unorm };
-
 	wgpu::FragmentState fragmentState{ .module = shaderModule,
 									  .entryPoint = "fragmentMain",
 									  .targetCount = 1,
@@ -106,12 +216,6 @@ void Render()
 	device.GetQueue().Submit(1, &commands);
 }
 
-void InitGraphics(wgpu::Surface surface)
-{
-	SetupSwapChain(surface);
-	CreateRenderPipeline();
-}
-
 void Start()
 {
 	if (!glfwInit())
@@ -120,8 +224,7 @@ void Start()
 	}
 
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window =
-		glfwCreateWindow(kWidth, kHeight, "WebGPU window", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(kWidth, kHeight, "WebGPU window", nullptr, nullptr);
 
 #if defined(__EMSCRIPTEN__)
 	wgpu::SurfaceDescriptorFromCanvasHTMLSelector canvasDesc{};
@@ -133,11 +236,13 @@ void Start()
 	wgpu::Surface surface = wgpu::glfw::CreateSurfaceForWindow(instance, window);
 #endif
 
-	InitGraphics(surface);
+	SetupSwapChain(surface);
+	CreateRenderPipeline();
 
 #if defined(__EMSCRIPTEN__)
 	emscripten_set_main_loop(Render, 0, false);
 #else
+	glfwSetKeyCallback(window, key_callback);
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
@@ -149,6 +254,9 @@ void Start()
 
 int main()
 {
+	printf("Hello\n");
+	sort_bench();
+
 	instance = wgpu::CreateInstance();
 	GetDevice([](wgpu::Device dev)
 		{
---
+++ todo.md
@@ -0,0 +1,9 @@
+# Learn path
+
+1. - [ ] load shader from files
+2. - [ ] wgsl shader language
+   1. - [ ] import?
+   2. - [ ] resource binding
+3. - [ ] shader loader
+4. - [ ] data streaming
+5. - [x] wasm multi thread: not an option
